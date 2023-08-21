0825ba94140bc27d9b82c742c063486536713279
Anushka Singh <97422676+singhanushka0214@users.noreply.github.com>
2023-08-21 15:52:37 +0530

    Update invoices-per-country.sql

--- invoices-per-country.sql
+++ invoices-per-country.sql
@@ -1,5 +1,8 @@
-SELECT co.country_name, count(*), AVG(i.total_price)
-FROM country co, city ci, customer cu, invoice i
-WHERE co.id = ci.country_id AND ci.id = cu.city_id AND cu.id = i.customer_id
-GROUP BY co.country name
-HAVING AVG(i.total_price) > (SELECT AVG(total price))
+SELECT co.country_name, COUNT(*), AVG(i.total_price)
+FROM country co
+JOIN city ci ON co.id = ci.country_id
+JOIN customer cu ON ci.id = cu.city_id
+JOIN invoice i ON cu.id = i.customer_id
+GROUP BY co.country_name
+HAVING AVG(i.total_price) > (SELECT AVG(total_price) FROM invoice);
+
